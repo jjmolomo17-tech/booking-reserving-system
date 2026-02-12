0e2422e08e9df0b80f12cc615158df41642e38ea
Joy Molomo <jjmolomo17@gmail.com>
2026-02-12 23:09:57 +0200

    userRoutes.js edit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,3 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userCntroller');
 
 router.post('/register' , userController.createUser);
+router.get('/users' , userController.getUser);
+
+module.exports = router;
