9cbdcf84429d8caa0d2c2d389afcb32de9a7100e
Joy Molomo <jjmolomo17@gmail.com>
2026-02-12 22:07:46 +0200

    import in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('/routes/userRoutes');
 
+app.use('/api', userRoutes);
 app.use(express.json());
 
 const PORT = 3000;
