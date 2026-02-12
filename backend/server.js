291cf96cef3aede844007355887f97761b96e271
Joy Molomo <jjmolomo17@gmail.com>
2026-02-12 22:36:21 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const userRoutes = require('/routes/userRoutes');
 
-app.use('/api', userRoutes);
-app.use(express.json());
 
 const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/api', userRoutes);
+
+
+app.get('/api' , userRoutes);
+
+app.get ('/' , (req, res) => {
+    res.send('Welcome to the API')
+}); 
+
 
 app.listen(PORT, () => {
     console.log('Server is running.')
